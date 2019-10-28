0df0d2e8f4b06bfa4f843cd1f4e894aedab4a9b9
gasduster99 <grunloh@soe.ucsc.edu>
2019-10-27 19:51:34 -0700

    some work on 6.10BH.r

--- nick/toolbox/6.10BH.r
+++ nick/toolbox/6.10BH.r
@@ -7,6 +7,16 @@ library(RColorBrewer)
 #FUNCTIONS
 #
 
+#
+BHSpawn = function(S, a, b){
+	#S : spawning biomass
+	#a : BH a parameter
+	#b : BH b parameter
+	
+	#
+	return( a*S/(1+b*S) )
+}
+
 #allometric relationship between length and weight
 LtoW = function(l, rho){ rho*l^3 }
 
@@ -59,34 +69,39 @@ L00 = 50
 t0  = 0
 M   = 0.1
 f   = 0.1
+#
+ar  = 3 
+bhA = 3
+bhB = 0.002
 
 #
-#PART ONE
-#
-
-#
-N = matrix(NA, nrow=Tea, ncol=A)
+N = matrix(0, nrow=Tea, ncol=A)
 colnames(N) = sprintf("AGE %d", 0:9)
 rownames(N) = sprintf("TIME %d", 1:Tea)
 N[1,1] = 500000
+#for(a in 1:(A-1)){ N[1,a+1]=knifeStep(a, N[1,a], ar, f) }
+
 ##
 #Y = matrix(0, nrow=A, ncol=3)
 #rownames(Y) = sprintf("AGE %d", 0:9)
 #colnames(Y) = sprintf("ar%d", 2:4)
 
 #iterate
-for(t in 1:Tea){
+for(t in 1:(Tea-1)){
 	for(a in 1:(A-1)){
-		#
-		N[t+1,1] = knifeStep(a, N[a,1], 2, f)	
+		#Live and Die in LA
+		N[t+1,a+1] = knifeStep(a, N[t,a], ar, f)		
 	}
+	#Reproduce
+	S = LtoW(vbAtoL((ar+1):A, t0, k, L00), rho)%*%N[t,(ar+1):A]
+	N[t+1,1] = BHSpawn(S, bhA, bhB)
 }
 
-#plot
-cols = brewer.pal(9, "Set1")
-matplot(N, type='l', col=cols[1:3], lwd=3)
-dev.new()
-matplot(Y, type='l', col=cols[1:3], lwd=3)
+##plot
+#cols = brewer.pal(9, "Set1")
+#matplot(N, type='l', col=cols[1:3], lwd=3)
+#dev.new()
+#matplot(Y, type='l', col=cols[1:3], lwd=3)
 
 ##
 ##PART TWO
