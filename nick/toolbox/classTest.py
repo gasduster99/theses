81a5d927d0cf2fbac4621ca72aac2fa359dd101c
gasduster99 <grunloh@soe.ucsc.edu>
2019-11-19 22:54:39 -0800

    fiddling with python version

--- nick/toolbox/classTest.py
+++ nick/toolbox/classTest.py
@@ -9,32 +9,16 @@ from ageStruct import *
 #FUNCTIONS
 #
 
-#
-def dNdt(t, y, params):
+#must start with t, y, followed by *args or **kwargs
+def dNdt(t, y, af, nm, fm):
 	#
-	
+	out = y*(exp(-nm)-1)
+        if t>=af: out = y*(exp(-nm-fm)-1)
+	#
+	return( out )
 #
 
-
-dNdt = function(t, y, params){
-        #params[1] : af age suseptible to fishing
-        #params[2] : nm natural mortality
-        #params[3] : fm fishing mortality
-
-        #unpack
-        af = params[1]
-        nm = params[2]
-        fm = params[3]
-        #assume population is not in the fishery, but if it is include fishing mortality
-        out = y*(exp(-nm)-1)
-        if(t>=af){ out = y*(exp(-nm-fm)-1) }
-        #
-        return( list(out) )
-}
-
-
-
-#
-cats = np.arange(10)
-mod  = ageModel(0, 0, 0, 0, cats)
+##
+#cats = np.arange(10)
+#mod  = ageModel(0, 0, 0, 0, cats)
 
