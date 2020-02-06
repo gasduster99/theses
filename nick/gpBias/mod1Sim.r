ea143c785ec764d199edec1b76c0f829b09a7afe
gasduster99 <grunloh@soe.ucsc.edu>
2020-02-06 14:10:52 -0800

    playing with GA for roots

--- nick/gpBias/mod1Sim.r
+++ nick/gpBias/mod1Sim.r
@@ -1,6 +1,7 @@
 rm(list=ls())
 
 #
+library(GA)
 library(boot)
 library(rootSolve)
 #
@@ -79,54 +80,93 @@ zetaSims = seq(0.2, 0.6, 0.1)
 xiSims = seq(0.5, 3, 0.5)
 
 #
-Fs = M*1.2 #*xiSims[1]
-hs = 1-exp(-Fs)
-
-#
-B0 = 2500
-Bs = B0*0.5#zetaSims[1]
-
-#
-f = function(x, pars){
-	#
-	Cs    = x[1]
-	gamma = x[2]
-	#
-	delta = pars$delta
-	lhs   = pars$lhs
-	B0    = pars$B0
-	Bs    = pars$Bs 
-	#
-	N0  = N0Funk(delta, Cs, lhs, gamma)
-	Nhs = NhsFunk(delta, Cs, lhs, gamma)
-	#
-	return( c(Nhs-Bs, N0-B0) )
+for(zi in 1:length(zetaSims)){
+	for(xj in 1:length(xiSims)){
+		#
+		Fs = M*xiSims[xj]
+		hs = 1-exp(-Fs)
+		
+		#
+		B0 = 3000
+		Bs = B0*zetaSims[zi]
+		
+		#
+		fGA = function(x, pars){
+			#
+			Cs    = x[1]
+			gamma = x[2]
+			#
+			delta = pars$delta
+			lhs   = pars$lhs
+			B0    = pars$B0
+			Bs    = pars$Bs 
+			#
+			N0  = N0Funk(delta, Cs, lhs, gamma)
+			Nhs = NhsFunk(delta, Cs, lhs, gamma)
+			#
+			return( -log(sum(c(Nhs-Bs, N0-B0)^2)) )
+		}
+		
+		#
+		par = data.frame(delta=1-exp(-M), lhs=logit(hs), B0=B0, Bs=Bs)
+		CGga = ga(type    = 'real-valued',
+		        fitness = fGA, pars=par,
+		        lower   = c(0, -10),
+		        upper   = c(1000, 10),
+		        #popSize = 1e4, ##set[['popSize']], #gaBoost[['popSize']], 
+		        maxiter = 1e3, #set[['maxiter']], #gaBoost[['maxiter']],
+		        run     = 100, #set[['run']],     #gaBoost[['run']],
+		        optim   = T,
+		        parallel= T #set[['parallel']],        #T,
+		        #monitor = F, 
+		        #suggestions = start
+		)
+		
+		#
+		f = function(x, pars){
+			#
+			Cs    = x[1]
+			gamma = x[2]
+			#
+			delta = pars$delta
+			lhs   = pars$lhs
+			B0    = pars$B0
+			Bs    = pars$Bs
+			#
+			N0  = N0Funk(delta, Cs, lhs, gamma)
+			Nhs = NhsFunk(delta, Cs, lhs, gamma)
+			#
+			return( c(Nhs-Bs, N0-B0) )
+		}
+		
+		#
+		start = CGga@solution #c(300,1)#
+		CG = multiroot(f, start, parms=par,
+			maxiter=1e6#,
+			#rtol=.Machine$double.eps,
+			#atol=.Machine$double.eps,
+			#ctol=.Machine$double.eps
+		)$root
+		
+		#
+		datGen = prodModel$new(dNdt=dNdt, time=1:TT, N0Funk=N0Funk, delta=1-exp(-M), catch=catch,
+			lhs 	= logit(hs),
+			Cs 	= CG[1],	
+			gamma	= CG[2],
+			lq	= log(0.0004),
+			sdo	= 0.1
+		)
+		datGen$iterate()
+		dev.new()
+		datGen$plotMean()
+		#
+		cv=0.01
+		datGen$sdo = mean(datGen$N)*cv
+		cpue = rlnorm(TT, datGen$lq+log(datGen$N), datGen$sdo)
+		points(cpue)
+		##
+		#dev.new()
+		#plot(cpue, ylim=c(0, max(cpue)*1.1))
+		#datGen$plotMean(add=T, col='red')
+	}
 }
-
-#
-start = c(300, -1)
-par = data.frame(delta=1-exp(-M), lhs=logit(hs), B0=B0, Bs=Bs)
-CG = multiroot(f, start, parms=par,
-	maxiter=10^5,
-	rtol=.Machine$double.eps,
-	atol=.Machine$double.eps,
-	ctol=.Machine$double.eps
-)$root
-
-#
-datGen = prodModel$new(dNdt=dNdt, time=1:TT, N0Funk=N0Funk, delta=1-exp(-M), catch=catch,
-	lhs 	= logit(hs),
-	Cs 	= CG[1],	
-	gamma	= CG[2],
-	lq	= log(0.0004),
-	sdo	= 0.1
-)
-datGen$iterate()
-datGen$plotMean()
-cpue = rlnorm(TT, datGen$lq+log(datGen$N), datGen$sdo)
-points(cpue)
-##
-#dev.new()
-#plot(cpue, ylim=c(0, max(cpue)*1.1))
-#datGen$plotMean(add=T, col='red')
-
