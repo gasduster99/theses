0e9e173181ecec828e8065ff343c148cfaace985
nick <nick@SWC-MALUS-ML.local>
2021-04-15 10:50:48 -0700

    derizo fail

--- nick/gpBias/derizoTry.r
+++ nick/gpBias/derizoTry.r
@@ -13,37 +13,25 @@ dPdt = function(t, P, alpha, beta, gamma, M, catch){
 	#
 	C = catch[t]
 	#
-	R = alpha*P/(1+beta*P^(1/gamma))
+	R = alpha*P/(1+beta*P)^(gamma)
 	out = R - M*P - C #(MM+FF)*P
 	#
 	return( list(out) )
 }
 
 #
-shepSRR = function(P, alpha, beta, gamma){ alpha*P/(1+beta*P^(1/gamma)) }
+derizoSRR = function(P, alpha, beta, gamma){ alpha*P/(1+beta*P)^(gamma) }
 
 #
-PBar = function(ff, alpha, beta, gamma, M){ exp(gamma*log(alpha/(M+ff)-1) - gamma*log(beta)) }
-#(alpha/(M+ff)-1)^gamma * beta^-gamma }
+PBar = function(ff, alpha, beta, gamma, M){ ((alpha/(M+ff))^(1/gamma)-1) * 1/beta }
 
 #
 FMsy = function(alpha, beta, gamma, M){
 	#
-	FUpper = alpha-M
-	out = uniroot.all(function(ff){ 1 - exp(log(ff)+log(alpha)+log(gamma) - (2*log(M+ff)+log(alpha/(M+ff)-1))) }, c(0, FUpper))	#
-	#uniroot: return(out$root)
-	return(out[1])
-}
-
-#
-getBeta = function(alpha, gamma, M, P0, start=0.01){ 
-	optim(start, function(x){ 
-			(PBar(0, alpha, x, gamma, M)-P0)^2 
-		}, 
-		method='Brent', 
-		lower=0, 
-		upper=10^6
-	)$par
+	#FUpper = alpha-M
+	out = uniroot.all(function(ff){ 1 - (alpha/(M+ff))^(-1/gamma) - (ff/gamma)*(1/(M+ff)) }, c(0, 10))
+	#
+	return(out)	
 }
 
 #
@@ -57,17 +45,17 @@ M = 0.2
 P0 = 3000
 
 #               
-xi = 2.4	#2          #3.4    #1.1
-zeta = 0.35	#0.25     #0.74   #0.6
+xi = 3		#2          #3.4    #1.1
+zeta = 0.25	#0.25     #0.74   #0.6
 
 #
 #MODELS
 #
 
 #
-alpha = 2
+alpha = 1
 gamma = 1
-beta = getBeta(alpha, gamma, M, P0)
+beta = 0.001 #getBeta(alpha, gamma, M, P0)
 #
 FStar = FMsy(alpha, beta, gamma, M)
 PStar = PBar(FStar, alpha, beta, gamma, M)
@@ -75,41 +63,43 @@ PZero = PBar(0, alpha, beta, gamma, M)
 
 #
 f = function(par, extra){
-       #unpack
-       alpha = par[1] 
-       gamma = par[2]
-       #extra
-       M = extra$M
-       xi = extra$xi
-       zeta = extra$zeta
-       
-       #solve beta against P0
-       beta = getBeta(alpha, gamma, M, extra$P0) 
+       	#unpack
+       	alpha = par[1] 
+       	beta  = par[2]
+	gamma = par[3]
+       	#extra
+       	M = extra$M
+       	xi = extra$xi
+       	zeta = extra$zeta
+       	
+       	#par values
+       	FStar = FMsy(alpha, beta, gamma, M) 
+       	PStar = PBar(FStar, alpha, beta, gamma, M)
+	PZero = PBar(0, alpha, beta, gamma, M)
+	#ref values
+       	FSRef = xi*M
+       	PSRef = zeta*extra$P0
+	PZRef = extra$P0
 
-       #par values
-       FStar = FMsy(alpha, beta, gamma, M) 
-       PStar = PBar(FStar, alpha, beta, gamma, M)
-       #ref values
-       FSRef = xi*M
-       PSRef = zeta*extra$P0
-
-       #
-       out = c(FStar-FSRef, PStar-PSRef)
-       return( out )
+       	#
+       	out = c(FStar-FSRef, PStar-PSRef, PZero-PZRef)
+       	return( out )
 }
 
 #
-par = c(alpha, gamma)
+par = c(alpha, beta, gamma)
 extra = data.frame(xi=xi, zeta=zeta, M=M, P0=P0)
-
 #
+lower   = c(0, 0, 0)
+upper   = c(100, 5, 5)
+vol = prod(upper-lower)
 gaRoot = ga(
 	type    = 'real-valued',
         fitness = function(x, extra){ -sum(f(x, extra)^2) }, 
 	extra   = extra,
-        lower   = c(0, 0),
-        upper   = c(5, 5),
-        popSize = 100,
+        lower   = lower,
+        upper   = upper,
+        popSize = 2000, #*vol,
         maxiter = 1e6,
         run     = 500,
         optim   = T,
@@ -118,12 +108,25 @@ gaRoot = ga(
 par = gaRoot@solution
 
 #
+alphaG = par[1] #roots$root[1]
+gammaG = par[3] #roots$root[2]
+betaG  = par[2] #getBeta(alphaR, gammaR, M, P0)
+#
+FStarG = FMsy(alphaG, betaG, gammaG, M)
+PStarG = PBar(FStarG, alphaG, betaG, gammaG, M)
+PZeroG = PBar(0, alphaG, betaG, gammaG, M)
+
+#
+thresh = 0.05
+isNormG = norm(matrix(c(FStarG/M-xi, PStarG/PZeroG-zeta), ncol=2))/norm(matrix(c(xi, zeta), ncol=2))<thresh
+
+#
 roots = multiroot(f, par, parms=extra, positive=T)#, maxiter=1e6)
 
 #
 alphaR = roots$root[1]
-gammaR = roots$root[2]
-betaR  = getBeta(alphaR, gammaR, M, P0)
+gammaR = roots$root[3]
+betaR  = roots$root[2]
 #
 FStarR = FMsy(alphaR, betaR, gammaR, M)
 PStarR = PBar(FStarR, alphaR, betaR, gammaR, M)
@@ -131,7 +134,42 @@ PZeroR = PBar(0, alphaR, betaR, gammaR, M)
 
 #
 thresh = 0.05
-isNorm = norm(matrix(c(FStarR/M-xi, PStarR/PZeroR-zeta), ncol=2))/norm(matrix(c(xi, zeta), ncol=2))<thresh
+isNormR = norm(matrix(c(FStarR/M-xi, PStarR/PZeroR-zeta), ncol=2))/norm(matrix(c(xi, zeta), ncol=2))<thresh
+
+
+
+
+
+
+
+
+
+#
+#JUNK
+#
+
+##
+#abgRootFind = function(){
+#	#alpha>M+ff
+#	FUpper = alpha-M
+#	##(ff*alpha*gamma)/((M+ff)^2*(alpha/(M+ff)-1)) }
+#	FStar = uniroot.all(function(ff){ 1 - exp(log(ff)+log(alpha)+log(gamma) - (2*log(M+ff)+log(alpha/(M+ff)-1))) }, c(0, FUpper)) 
+#	PStar = PBar(FStar, alpha, beta, gamma, M)
+#	P0 = PBar(0, alpha, beta, gamma, M)	
+#}
+
+##alpha>M+ff
+#FUpper = alpha-M
+##FStar = uniroot.all(function(ff){ (ff*alpha*gamma)/((M+ff)^2*(alpha/(M+ff)-1)) }, c(0, FUpper))
+#FStar = uniroot.all(function(ff){ 1 - exp(log(ff)+log(alpha)+log(gamma) - (2*log(M+ff)+log(alpha/(M+ff)-1))) }, c(0, FUpper)) 
+#logFStar = uniroot.all(function(lff){ 1 - exp(lff+log(alpha)+log(gamma) - (2*log(M+exp(lff))+log(alpha/(M+exp(lff))-1))) }, c(0, log(FUpper)))
+#FStar = exp(logFStar)
+
+
+
+
+
+
 
 ##
 #fga = function(par, extra){
@@ -252,39 +290,39 @@ isNorm = norm(matrix(c(FStarR/M-xi, PStarR/PZeroR-zeta), ncol=2))/norm(matrix(c(
 #PZeroR = PBar(0, alphaR, betaR, gammaR, M)
 ##
 
+#out = optim(2*M, function(ff){ 
+	#		-exp(ff)*PBar(exp(ff), alpha, beta, gamma, M) 
+	#	},
+	#	#gr = function(ff){
+	#	#	-PBar(exp())
+	#	#}
+	#	method='Brent',
+	#	lower=-10,
+	#	upper=10 #FUpper 
+	#)
+	#uniroot.all(function(ff){ 1 - exp(log(ff)+log(alpha)+log(gamma) - (2*log(M+ff)+log(alpha/(M+ff)-1))) }, c(0, FUpper))	#
+	#uniroot: return(out$root)
+	#return(exp(out$par))
 
-
-
-
-
-
-
-
-#
-#JUNK
-#
 
 ##
-#abgRootFind = function(){
-#	#alpha>M+ff
-#	FUpper = alpha-M
-#	##(ff*alpha*gamma)/((M+ff)^2*(alpha/(M+ff)-1)) }
-#	FStar = uniroot.all(function(ff){ 1 - exp(log(ff)+log(alpha)+log(gamma) - (2*log(M+ff)+log(alpha/(M+ff)-1))) }, c(0, FUpper)) 
-#	PStar = PBar(FStar, alpha, beta, gamma, M)
-#	P0 = PBar(0, alpha, beta, gamma, M)	
+#getBeta = function(alpha, gamma, M, P0, start=0.01){ 
+#	optim(start, function(x){ 
+#			(PBar(0, alpha, x, gamma, M)-P0)^2 
+#		}, 
+#		method='Brent', 
+#		lower=0, 
+#		upper=10^6
+#	)$par
 #}
 
-##alpha>M+ff
-#FUpper = alpha-M
-##FStar = uniroot.all(function(ff){ (ff*alpha*gamma)/((M+ff)^2*(alpha/(M+ff)-1)) }, c(0, FUpper))
-#FStar = uniroot.all(function(ff){ 1 - exp(log(ff)+log(alpha)+log(gamma) - (2*log(M+ff)+log(alpha/(M+ff)-1))) }, c(0, FUpper)) 
-#logFStar = uniroot.all(function(lff){ 1 - exp(lff+log(alpha)+log(gamma) - (2*log(M+exp(lff))+log(alpha/(M+exp(lff))-1))) }, c(0, log(FUpper)))
-#FStar = exp(logFStar)
 
-
-
-
-
-
+##
+#lower   = c(M, 0, 0)
+#upper   = c(10, 5, 5)
+#jdeRoot = JDEoptim(lower, upper, 
+#	function(x, extra){ -sum(f(x, extra)^2) },
+#	extra = extra
+#)
 
 
