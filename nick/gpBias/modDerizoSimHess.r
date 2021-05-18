909f00cf19d5c982e96246c4d23546f5c0087847
gasduster99 <grunloh@soe.ucsc.edu>
2021-05-17 17:49:40 -0700

    initial derizo runs

--- nick/gpBias/modDerizoSimHess.r
+++ nick/gpBias/modDerizoSimHess.r
@@ -326,7 +326,7 @@ beta = getBeta(alpha, gamma, M, P0)
 ##layout(matrix(1:(length(zetaSims)*length(xiSims)), nrow=length(zetaSims), ncol=length(xiSims), byrow=T))
 #
 #
-registerDoParallel(8)
+registerDoParallel(48)
 opts = list(preschedule=F)
 foreach(i=1:length(zetaSims), .options.multicore = opts) %dopar% {
 #for(i in 1:length(zetaSims)){
