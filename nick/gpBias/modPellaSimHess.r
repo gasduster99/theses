9610dd1a43177d0a98db47edea205379e9698669
gasduster99 <grunloh@soe.ucsc.edu>
2021-06-17 09:46:24 -0700

    hoping to have filled in some spaces

--- nick/gpBias/modPellaSimHess.r
+++ nick/gpBias/modPellaSimHess.r
@@ -207,8 +207,8 @@ P0 = 6000 #3000
 place = './modsPellaFineQFixReduxP06000/'
 
 #grid for simulation
-zetaSims = (seq(0.1, 0.7, 0.01)) #rev(seq(0.15, 0.7, 0.01)) 	#rev(seq(0.1, 0.8, 0.05)) #rev(seq(0.1, 0.8, 0.01)) 	
-xiSims =   rev(seq(0.5, 4.5, 0.05))  #rev(seq(0.5, 3.5, 0.05)) 		#c(seq(0.5, 3.5, 0.25)) #rev(seq(0.5, 3.5, 0.05))	
+zetaSims = rev(seq(0.1, 0.9, 0.01)) #rev(seq(0.15, 0.7, 0.01)) 	#rev(seq(0.1, 0.8, 0.05)) #rev(seq(0.1, 0.8, 0.01)) 	
+xiSims =   (seq(0.5, 4.5, 0.05))  #rev(seq(0.5, 3.5, 0.05)) 		#c(seq(0.5, 3.5, 0.25)) #rev(seq(0.5, 3.5, 0.05))	
 
 #start the parameters here
 alpha = 1
@@ -220,7 +220,7 @@ beta  = P0
 #layout(matrix(1:(length(zetaSims)*length(xiSims)), nrow=length(zetaSims), ncol=length(xiSims), byrow=T))
 
 #
-registerDoParallel(48)
+registerDoParallel(20)
 opts = list(preschedule=F)
 foreach(i=1:length(zetaSims), .options.multicore = opts) %dopar% {
 #for(i in 1:length(zetaSims)){
