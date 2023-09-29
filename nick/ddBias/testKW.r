631b2f7d071c1fb50901e1e1679f2b9ff3c89657
gasduster99 <grunloh@soe.ucsc.edu>
2023-09-29 13:10:51 -0700

    a bunch of flat data to explotre the shape of growth

--- nick/ddBias/testKW.r
+++ nick/ddBias/testKW.r
@@ -177,7 +177,7 @@ odeMethod = "lsode" #"radau" #
 datFiles = sprintf("%s%s", place, list.files(path=place, pattern=glob2rx("datGen*.rda")))
 
 #
-i = 65 #15
+i = 126 #(3.19, 0.487) #66 yes (1.767, 0.48) #65 yes #15 no (0.577, 0.619) #67 no (1.79, 0.608)
 ##
 #registerDoParallel(7) #46)
 #opts = list(preschedule=F)
@@ -322,7 +322,7 @@ i = 65 #15
                 c('lsdo', 'lalpha', 'lbeta', 'kappa', 'WW'),
                 lower   = c(log(0.001), log(M+kappa), -10, kappa/5, WW/5),
                 upper   = c(log(1), log(100), -2, 2*kappa, 2*WW),      #log(getBeta(100, -1, M, P0))
-                gaBoost = list(run=100, parallel=T, popSize=10^3),#10^4),
+                gaBoost = list(run=10, parallel=T, popSize=10^3),#10^4),
                 persistFor = 5,
                 fitQ    = F
         )
